b8a0d4d98dbba3e784f42b23e4ff6bf6c1fe152c
Bjorne1 <35253647+Bjorne1@users.noreply.github.com>
2019-05-09 16:57:13 +0800

    add eureka client

--- tensquare_search/src/main/java/com/tensquare/search/SearchApplication.java
+++ tensquare_search/src/main/java/com/tensquare/search/SearchApplication.java
@@ -2,6 +2,7 @@ package com.tensquare.search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import util.IdWorker;
 
@@ -11,6 +12,7 @@ import util.IdWorker;
  * @Date: Created in 2019/5/9 9:58
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class SearchApplication {
     public static void main(String[] args) {
         SpringApplication.run(SearchApplication.class, args);
--- tensquare_sms/src/main/java/com/tensquare/sms/SmsApplication.java
+++ tensquare_sms/src/main/java/com/tensquare/sms/SmsApplication.java
@@ -2,6 +2,7 @@ package com.tensquare.sms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Description:
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date: Created in 2019/5/9 11:45
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class SmsApplication {
     public static void main(String[] args) {
         SpringApplication.run(SmsApplication.class, args);
