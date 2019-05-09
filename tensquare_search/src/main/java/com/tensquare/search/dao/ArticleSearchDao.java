4c1a6326b942c041001a503a447b34881a073e97
Bjorne1 <35253647+Bjorne1@users.noreply.github.com>
2019-05-09 10:18:41 +0800

    add elasticsearch module

---
+++ tensquare_search/src/main/java/com/tensquare/search/dao/ArticleSearchDao.java
@@ -0,0 +1,23 @@
+package com.tensquare.search.dao;
+
+import com.tensquare.search.pojo.Article;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * @Description:
+ * @Author: WenChangSheng
+ * @Date: Created in 2019/5/9 10:09
+ */
+public interface ArticleSearchDao extends ElasticsearchRepository<Article, String> {
+
+    /**
+     * 检索
+     *
+     * @param title,content
+     * @return Article
+     */
+    public Page<Article> findByTitleOrContentLike(String title, String
+            content, Pageable pageable);
+}
