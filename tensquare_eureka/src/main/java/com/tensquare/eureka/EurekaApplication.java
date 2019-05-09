e79755f3ae2ec9862d321abaad969a70e584a254
Bjorne1 <35253647+Bjorne1@users.noreply.github.com>
2019-05-09 16:39:22 +0800

    add eureka server

---
+++ tensquare_eureka/src/main/java/com/tensquare/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.tensquare.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Description:
+ * @Author: WenChangSheng
+ * @Date: Created in 2019/5/9 16:30
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
